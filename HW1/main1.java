064ac4af196e62aeb9cedadfc5869d67e2eebb8c
Stephany <stephanyb81@gmail.com>
2018-01-31 17:48:39 -0500

    Update main1.java

--- HW1/main1.java
+++ HW1/main1.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
+import java.lang.Math;
 
-
-public class Q1 {
-
-	public static void main(String[] args) {
+public class Q2 
+{
+	public static void main(String[] args)
+	{
 		double area = 0.0, radius = 0.0;
-		
 		Scanner input = new Scanner(System.in);
 		
-		area = input.nextDouble();
+		while(input.hasNextLine())
+		{
+		radius = input.nextDouble();
+		area = CalcR(radius);
+		
 		System.out.println(area);
 		
+		}
 		input.close();
 	}
-
-}
+	public static double CalcR(double num)
+	{
+		num = (Math.PI)*Math.pow(num , 2.0);
+		return num;
+	}
+	
+}											
