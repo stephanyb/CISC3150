56c4eb7ff1f1ed9826e8511ac74898056a7830c1
Stephany <stephanyb81@gmail.com>
2018-01-30 23:37:48 -0500

    Create main1.java

---
+++ HW1/main1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		double area = 0.0, radius = 0.0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		area = input.nextDouble();
+		System.out.println(area);
+		
+		input.close();
+	}
+
+}
